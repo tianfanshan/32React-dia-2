59f52a2bf8ef24948a6be4ad33dbfa0b0ff32f75
tianfanshan <stf30111994@gmail.com>
2022-06-14 22:07:05 +0200

    ruta de imagen resulvido

--- restaurante/src/App.jsx
+++ restaurante/src/App.jsx
@@ -13,21 +13,21 @@ function App() {
       id:1,
       name:'Empanadilla china',
       description:'Comida tradicional de China',
-      img:'http://ali.xinshipu.cn/20120517/original/1337242483632.jpg',
+      img:'./assets/1337242483632.jpg',
       price:10
     },
     {
       id:2,
       name:'Pasta china muy picante de ChongQing',
       description:'En la ciudad de ChonQing casi todos comen eso al dia',
-      img:'https://i2.yanglegu.com/yanglegu/img/img/7056161ec3b40.jpg',
+      img:'./assets/2021100716336175861992868819358.jpg',
       price:20
     },
     {
       id:3,
       name:'Fideo muy popular de GuangDong',
       description:'Cuando andas por la ciudad de GuanDong por la calle,encuentras por todas partes esta comida tipica de la ciudad',
-      img:'https://img.alicdn.com/bao/uploaded/i1/123678513/O1CN017E34e62Cl0IYOyukd_!!123678513.jpg',
+      img:'./assets/O1CN017E34e62Cl0IYOyukd_!!123678513.jpg',
       price:15
     }
   ]
